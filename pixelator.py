057e55838896dd823c744ae26447f35dfacd7e28
Thomas Jenny <thomas.jenny.90@gmail.com>
2025-01-11 18:44:22 +0100

    Pixelator functionality and unit test

--- pixelator.py
+++ pixelator.py
@@ -1,33 +1,24 @@
 import json
-# import matplotlib.pyplot as plt
 import numpy as np
-import os
 from PIL import Image, ImageColor
-from typing import Dict, List
 
 
 class Pixelator:
-    """Pixelator class
+    """A class to create pixelated versions of images using various
+    color palettes.
 
     Attributes:
+        palettes_file_path (str, optional): path to the color palattes
+            file used to recolor the image.
     """
 
-    # def __init__(self, palettes: Dict[str, List[tuple]], img_path: str) -> None:
     def __init__(self, palettes_file_path: str = "assets/palettes_hex.json"):
-        """Initialize the Pixelator class
-
-        Args:
-            x
-        """
-        # self.img_path = img_path
         self.palettes_file_path = palettes_file_path
 
-    def get_palettes(self) -> Dict[str, List[tuple]]:
-        """Load all palettes with their hex color codes and convert them
-        to RGB.
-
-        Args:
-            x
+    def get_palettes(self) -> dict[str, list[tuple]]:
+        """Load all palettes with their hex color codes, convert them
+        to RGB, and return the RGB palettes. Uses the default file path
+        as defined on class level.
         """
         rgb_palettes = {}
         palettes_file_path = self.palettes_file_path
@@ -42,59 +33,83 @@ class Pixelator:
 
         return rgb_palettes
 
-    def recolor_pixel(self, square: np.array, palette: str = ""):  # , palette: str) -> List:
+    def recolor_pixel(self, square: np.ndarray, palette: str = "") -> list[int]:
         """Calculates the average red, green, and blue values of all
-        pixels in a given square with size (square side length x square
-        side length).
+        pixels in a given square with the following size: square side
+        length * square side length.
+
+        Args:
+            square (np.ndarray): 2D numpy array where each element is
+                a pixel represented as an array of red, green, and blue
+                values.
+            palette (str, optional): name of the color palette to
+                recolor the pixel. If not specified, the average color
+                of all pixels in the square used (default).
+
+        Returns:
+            list[int]: list of three integers representing the new red,
+                green, and blue values.
         """
-        # Store each r, g, b value for the square
-        reds, blues, greens = [], [], []
+        # Prepare arrays for average color calculation
+        square_height = len(square)
+        square_width = len(square[0])
+        no_of_pixels_in_square = square_height * square_width
+
+        reds = np.zeros(no_of_pixels_in_square, dtype=np.int64)
+        greens = np.zeros(no_of_pixels_in_square, dtype=np.int64)
+        blues = np.zeros(no_of_pixels_in_square, dtype=np.int64)
+
+        # Store each r, g, b value in the square
+        index = 0
         for row in square:
             for pixel in row:
-                reds.append(pixel[0])
-                greens.append(pixel[1])
-                blues.append(pixel[2])
+                reds[index] = pixel[0]
+                greens[index] = pixel[1]
+                blues[index] = pixel[2]
+                index += 1
 
-        # Calculate the average r, g, b values
+        # Calculate average color
+        red_avg = int(np.sum(reds) / no_of_pixels_in_square)
+        green_avg = int(np.sum(greens) / no_of_pixels_in_square)
+        blue_avg = int(np.sum(blues) / no_of_pixels_in_square)
+        rgb_avg = [red_avg, green_avg, blue_avg]
 
-        ########################################
-        ########################################
-        ########################################
-
-        # HANDLE OVERFLOW WARNING HERE!
-
-        red_avg = int(sum(reds) / len(reds))
-        green_avg = int(sum(greens) / len(greens))
-        blue_avg = int(sum(blues) / len(blues))
-        RGB_avg = [red_avg, green_avg, blue_avg]
-        # print(RGB_avg)
-
-        ########################################
-        ########################################
-        ########################################
-
-        # Get the color palettes
+        # Get color palettes
         palettes = self.get_palettes()
-
+        # Calculate the index of the closest color in the palette
         if palette in palettes.keys():
             palette = palettes[palette]
-            color_index = np.linalg.norm(
-                np.array(RGB_avg) - np.array(palette), axis=1
+            closest_color_index = np.linalg.norm(
+                np.array(rgb_avg) - np.array(palette), axis=1
             ).argmin()
-            RGB_new = list(palette[color_index])
+            rgb_new = list(palette[closest_color_index])
         else:
-            RGB_new = RGB_avg
+            rgb_new = rgb_avg
 
-        return RGB_new
+        return rgb_new
 
-    def pixelate(self, image_path, pixel_size, palette = ""):
-        # Open the image and convert it to a np array
+    def pixelate(
+        self, image_path: str, pixel_size: int, palette: str = ""
+    ) -> Image.Image:
+        """Pixelates and recolors the image.
+
+        Args:
+            image_path (str): path to the image to be pixelated.
+            pixel_size (int): size of the pixels in the pixelated image
+            palette (str, optional): name of the color palette to
+                recolor the pixel. If not specified, the average color
+                of all pixels in the square used (default).
+
+        Returns:
+            Image.Image: the pixelated image.
+        """
+        # Convert image to numpy array
         with Image.open(image_path) as im:
             image_array = np.asarray(im)
-            # print(image_array)
 
-        # Create a numpy array for the pixelated image whose height and
-        # width are divisible by the output pixel size without remainder
+        # Create an empty numpy array for the pixelated image. If
+        # needed, cut off pixels so that image height and width fits
+        # pixel size n times without remainder.
         original_image_height, original_image_width, _ = image_array.shape
         new_image_height = original_image_height - (original_image_height % pixel_size)
         new_image_width = original_image_width - (original_image_width % pixel_size)
@@ -103,49 +118,19 @@ class Pixelator:
         # Iterate over the image in squares determined by pixel_size
         for i in range(0, new_image_height, pixel_size):
             for j in range(0, new_image_width, pixel_size):
-                # Extract a square
-                square = image_array[i:i + pixel_size, j:j + pixel_size]
-                # Calculate the new color for the square
+                # Extract a square and recolor
+                square = image_array[i : i + pixel_size, j : j + pixel_size]
                 RGB_new = self.recolor_pixel(square, palette)
-                # Add a square in the color of RGB_new to the np array
-                # for the new image
-                new_image[i:i + pixel_size, j:j + pixel_size] = RGB_new
+                # Add recolored square to the new image's numpy array
+                new_image[i : i + pixel_size, j : j + pixel_size] = RGB_new
 
-        # Convert the new image array to an image
         new_img = Image.fromarray(new_image)
 
         return new_img
 
 
 if __name__ == "__main__":
-    # print(Pixelator.__doc__)
-
+    image_path = "img/wave.jpg"
     pixelator = Pixelator()
-
-    # pixelator.pixelate("img/lamborghini_small.jpg", 5)
-
-    # palettes = pixelator.get_palettes()
-    # print(palettes)
-
-    image_path = "img/lamborghini_small.jpg"
-    # with Image.open(image_path) as im:
-    #    image_array = np.asarray(im)
-    
-    no_pal = pixelator.pixelate(image_path, 5, "pico-8")
-    # no_pal.show()
-
-    # original_image_height, original_image_width, _ = image_array.shape
-# 
-    # new_image_height = original_image_height - (original_image_height % 5)
-    # new_image_width = original_image_width - (original_image_width % 5)
-    # new_image = np.zeros((new_image_height, new_image_width, 3), dtype=np.uint8)
-# 
-    # for i in range(0, new_image_height, 5):
-    #     for j in range(0, new_image_width, 5):
-    #         # Extract a square
-    #         square = image_array[i : i + 5, j : j + 5]
-    #         break
-    # 
-    # print(square)
-    # RGB_new = pixelator.recolor_pixel(square)#, "shimmering_sunset")
-    # print(RGB_new)
+    test = pixelator.pixelate(image_path, 3, "shimmering_sunset")
+    test.show()
