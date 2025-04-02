28ff435aace0762e312ae800546cd39c8c0f076f
Thomas Jenny <thomas.jenny.90@gmail.com>
2025-04-02 21:15:55 +0200

    Move draw_palette function to utils file

--- utils.py
+++ utils.py
@@ -1,16 +1,13 @@
 import math
 import matplotlib.pyplot as plt
-import streamlit as st
-
-from pixelator import Pixelator
 
 
 def draw_palette(
     palette_name: str,
     palette: list[tuple],
     cols: int = 5,
-    rect_width: float = 0.4,
-    rect_height: float = 0.2,
+    rect_width: float = 0.3,
+    rect_height: float = 0.11,
 ) -> plt.Figure:
     """Generates and returns a matplotlib Figure displaying a color palette.
 
@@ -24,8 +21,8 @@ def draw_palette(
             consists of three integers representing red, green, and blue
             values (0-255).
         cols (int, optional): The number of columns in the grid. Defaults to 5.
-        rect_width (float, optional): The width of each rectangle in the grid. Defaults to 0.4.
-        rect_height (float, optional): The height of each rectangle in the grid. Defaults to 0.2.
+        rect_width (float, optional): The width of each rectangle in the grid. Defaults to 0.3.
+        rect_height (float, optional): The height of each rectangle in the grid. Defaults to 0.11.
 
     Returns:
         plt.Figure: A matplotlib Figure object containing the plotted palette.
@@ -49,11 +46,11 @@ def draw_palette(
             ax[rect_no].imshow([[palette[rect_no]]], norm=plt.Normalize(0, 255))
         else:
             ax[rect_no].imshow([[(255, 255, 255)]], norm=plt.Normalize(0, 255))
-            ax[rect_no].set_frame_on(False)
 
         if rect_no == 0:
             ax[rect_no].set_title(palette_name, loc="left", fontsize="x-small")
 
+        ax[rect_no].set_frame_on(False)
         ax[rect_no].set_xticks([])
         ax[rect_no].set_yticks([])
         ax[rect_no].set_aspect(rect_height / rect_width)
@@ -61,8 +58,9 @@ def draw_palette(
     return fig
 
 
-
 if __name__ == "__main__":
+    from pixelator import Pixelator
+    
     pixelator = Pixelator()
     palettes = pixelator.get_palettes("RGB")
 
